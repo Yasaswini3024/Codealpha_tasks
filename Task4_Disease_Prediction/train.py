1f6339f706b88b6d9cc2a4b24e8c33edd83cea92
Yasaswini3024 <yasaswinigaddam63@gmail.com>
2026-06-28 12:26:09 +0530

    Create train.py

---
+++ Task4_Disease_Prediction/train.py
@@ -0,0 +1,35 @@
+# train.py
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import joblib
+
+# Load dataset
+data = pd.read_csv("heart.csv")
+
+# Features and Target
+X = data.drop("target", axis=1)
+y = data["target"]
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+# Predictions
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Model Accuracy:", accuracy)
+
+# Save model
+joblib.dump(model, "model.pkl")
+
+print("Model saved as model.pkl")
